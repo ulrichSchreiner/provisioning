09b13906cc81f260c59f927cb888f43c26bbef10
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2016-02-14 17:45:55 +0100

    provision my local pc

---
+++ README.md
@@ -0,0 +1,10 @@
+# Provisioning of my development machine
+
+The machine needs:
+
+ - to be a ubuntu/debian based system
+ - a running SSH daemon
+ - a running docker daemon
+ - my userid with sudo-rights
+
+Then simply call  `./run.sh`
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker run -it --rm \
+  -v $SSH_AUTH_SOCK:/ssh-agent \
+  --env SSH_AUTH_SOCK=/ssh-agent \
+  --net host \
+  -v `pwd`:/work ulrichschreiner/ansible-playbook \
+  -u `whoami` -v --ask-become-pass --ask-pass -i inventory playbook.yaml
