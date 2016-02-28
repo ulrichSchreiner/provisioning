27036ef28474945de4b1d3e4b93856c4edb645d8
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2016-02-28 16:30:23 +0100

    rename of ansible image

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker run -it --rm \
   -v $SSH_AUTH_SOCK:/ssh-agent \
   --env SSH_AUTH_SOCK=/ssh-agent \
   --net host \
-  -v `pwd`:/work ulrichschreiner/ansible-playbook \
+  -v `pwd`:/work ulrichschreiner/ansible playbook \
   -u `whoami` -v --ask-become-pass --ask-pass -i inventory playbook.yaml
