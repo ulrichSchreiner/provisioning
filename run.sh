eecfa5771c7a315d41a3c9b7c374507fa31085f8
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2016-12-14 11:53:30 +0100

    add zsh and refactor playbook

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm \
-  -v $SSH_AUTH_SOCK:/ssh-agent \
-  --env SSH_AUTH_SOCK=/ssh-agent \
-  --net host \
-  -v `pwd`:/work ulrichschreiner/ansible playbook \
-  -u `whoami` -vv --ask-become-pass --ask-pass -i inventory playbook.yaml
