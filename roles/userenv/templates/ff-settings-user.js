5e560d9831bf162e8d899e33c3836e8c0c4eb6ed
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2021-10-19 13:54:00 +0200

    install firefox-settings service

---
+++ roles/userenv/templates/ff-settings-user.js
@@ -0,0 +1,9 @@
+user_pref("browser.startup.homepage", "https://www.google.de");
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("accessibility.force_disabled", 1);
+user_pref("accessibility.typeaheadfind.enablesound", false);
+user_pref("gfx.webrender.all", true);
+user_pref("toolkit.zoomManager.zoomValues", ".3,.5,.67,.8,.85,.9,1,1.1,1.2,1.33,1.5,1.7,2,2.4,3,4,5");
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.rdd-vpx.enabled", false);
+user_pref("media.rdd-vpx.enabled", false);
